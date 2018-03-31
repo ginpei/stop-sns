2ee6531652bcaaf77a9117684708fed47b39d64b
ginpei <ginpei@gmail.com>
2018-03-31 13:00:10 -0700

    class

--- add-on/content_scripts/index.ts
+++ add-on/content_scripts/index.ts
@@ -7,78 +7,64 @@ namespace SnsBarrier {
     [anyObjKey: string]: any
   }
 
-  function kebabCase (source: string) {
-    return source.replace(/[A-Z]/g, v => `-${v.toLowerCase()}`);
-  }
-
-  function applyStyles (el: Element | null, styles: AnyObj) {
-    if (!el || !(el instanceof HTMLElement)) {
-      return;
+  class SnsBarrier {
+    async run () {
+      if (await this.isRunning()) {
+        this.showBarrier();
+      }
     }
 
-    const style = el.style as AnyObj;
-    Object.keys(styles).forEach((prop) => {
-      const value = styles[prop];
-      const kebabProp = kebabCase(prop);
-      style[kebabProp] = value;
-    });
+    async isRunning () {
+      // TODO find types
+      // @ts-ignore
+      const { running } = await browser.storage.local.get(['running']);
+      return running;
+    }
+
+    reset () {
+      const el = document.querySelector('#stopSns');
+      if (el && el.parentElement) {
+        el.parentElement.removeChild(el);
+      }
+      document.body.style.overflow = '';
+    }
+
+    showBarrier () {
+      this.reset();
+
+      const el = createElement(`
+        <div id="stopSns">
+          <div class="stopSns-content">
+            <h1 class="stopSns-message">Stop SNS</h1>
+            <div>
+              <button class="stopSns-ok">Unleash your lust</button>
+            </div>
+          </div>
+        </div>
+      `);
+
+      const okButton = el.querySelector('.stopSns-ok');
+      if (okButton) {
+        okButton.addEventListener('click', (event) => {
+          this.reset();
+        });
+      }
+
+      document.body.appendChild(el);
+      document.body.style.overflow = 'hidden';
+    }
   }
 
-  function createElement (html: string, styles?: object) {
+  function createElement (html: string) {
     const builder = document.createElement('div');
     builder.innerHTML = html.trim();
     const el = builder.firstElementChild as HTMLElement;
-    if (styles) {
-      applyStyles(el, styles);
-    }
+    // if (styles) {
+    //   applyStyles(el, styles);
+    // }
     return el;
   }
 
-  function reset () {
-    const el = document.querySelector('#stopSns');
-    if (el && el.parentElement) {
-      el.parentElement.removeChild(el);
-    }
-    document.body.style.overflow = '';
-  }
-
-  async function isRunning () {
-    // TODO find types
-    // @ts-ignore
-    const { running } = await browser.storage.local.get(['running']);
-    return running;
-  }
-
-  function showBarrier () {
-    reset();
-
-    const el = createElement(`
-      <div id="stopSns">
-        <div class="stopSns-content">
-          <h1 class="stopSns-message">Stop SNS</h1>
-          <div>
-            <button class="stopSns-ok">Unleash your lust</button>
-          </div>
-        </div>
-      </div>
-    `);
-
-    const okButton = el.querySelector('.stopSns-ok');
-    if (okButton) {
-      okButton.addEventListener('click', (event) => {
-        reset();
-      });
-    }
-
-    document.body.appendChild(el);
-    document.body.style.overflow = 'hidden';
-  }
-
-  async function main () {
-    if (await isRunning()) {
-      showBarrier();
-    }
-  }
-
-  main();
+  const controller = new SnsBarrier();
+  controller.run();
 }
