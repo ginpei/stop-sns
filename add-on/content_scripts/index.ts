360b473198b201096e41b8b7138bc11b4f2fe37d
ginpei <ginpei@gmail.com>
2018-03-31 12:55:42 -0700

    wrap with a namespace

--- add-on/content_scripts/index.ts
+++ add-on/content_scripts/index.ts
@@ -1,82 +1,84 @@
 'use strict';
 
-// in order to deal with obj[prop]. any better way?
-/* global AnyObj anyObjKey */
-interface AnyObj {
-  [anyObjKey: string]: any
-}
-
-function kebabCase (source: string) {
-  return source.replace(/[A-Z]/g, v => `-${v.toLowerCase()}`);
-}
-
-function applyStyles (el: Element | null, styles: AnyObj) {
-  if (!el || !(el instanceof HTMLElement)) {
-    return;
+namespace SnsBarrier {
+  // in order to deal with obj[prop]. any better way?
+  /* global AnyObj anyObjKey */
+  interface AnyObj {
+    [anyObjKey: string]: any
   }
 
-  const style = el.style as AnyObj;
-  Object.keys(styles).forEach((prop) => {
-    const value = styles[prop];
-    const kebabProp = kebabCase(prop);
-    style[kebabProp] = value;
-  });
-}
-
-function createElement (html: string, styles?: object) {
-  const builder = document.createElement('div');
-  builder.innerHTML = html.trim();
-  const el = builder.firstElementChild as HTMLElement;
-  if (styles) {
-    applyStyles(el, styles);
+  function kebabCase (source: string) {
+    return source.replace(/[A-Z]/g, v => `-${v.toLowerCase()}`);
   }
-  return el;
-}
 
-function reset () {
-  const el = document.querySelector('#stopSns');
-  if (el && el.parentElement) {
-    el.parentElement.removeChild(el);
-  }
-  document.body.style.overflow = '';
-}
+  function applyStyles (el: Element | null, styles: AnyObj) {
+    if (!el || !(el instanceof HTMLElement)) {
+      return;
+    }
 
-async function isRunning () {
-  // TODO find types
-  // @ts-ignore
-  const { running } = await browser.storage.local.get(['running']);
-  return running;
-}
-
-function showBarrier () {
-  reset();
-
-  const el = createElement(`
-    <div id="stopSns">
-      <div class="stopSns-content">
-        <h1 class="stopSns-message">Stop SNS</h1>
-        <div>
-          <button class="stopSns-ok">Unleash your lust</button>
-        </div>
-      </div>
-    </div>
-  `);
-
-  const okButton = el.querySelector('.stopSns-ok');
-  if (okButton) {
-    okButton.addEventListener('click', (event) => {
-      reset();
+    const style = el.style as AnyObj;
+    Object.keys(styles).forEach((prop) => {
+      const value = styles[prop];
+      const kebabProp = kebabCase(prop);
+      style[kebabProp] = value;
     });
   }
 
-  document.body.appendChild(el);
-  document.body.style.overflow = 'hidden';
-}
-
-async function main () {
-  if (await isRunning()) {
-    showBarrier();
+  function createElement (html: string, styles?: object) {
+    const builder = document.createElement('div');
+    builder.innerHTML = html.trim();
+    const el = builder.firstElementChild as HTMLElement;
+    if (styles) {
+      applyStyles(el, styles);
+    }
+    return el;
   }
-}
 
-main();
+  function reset () {
+    const el = document.querySelector('#stopSns');
+    if (el && el.parentElement) {
+      el.parentElement.removeChild(el);
+    }
+    document.body.style.overflow = '';
+  }
+
+  async function isRunning () {
+    // TODO find types
+    // @ts-ignore
+    const { running } = await browser.storage.local.get(['running']);
+    return running;
+  }
+
+  function showBarrier () {
+    reset();
+
+    const el = createElement(`
+      <div id="stopSns">
+        <div class="stopSns-content">
+          <h1 class="stopSns-message">Stop SNS</h1>
+          <div>
+            <button class="stopSns-ok">Unleash your lust</button>
+          </div>
+        </div>
+      </div>
+    `);
+
+    const okButton = el.querySelector('.stopSns-ok');
+    if (okButton) {
+      okButton.addEventListener('click', (event) => {
+        reset();
+      });
+    }
+
+    document.body.appendChild(el);
+    document.body.style.overflow = 'hidden';
+  }
+
+  async function main () {
+    if (await isRunning()) {
+      showBarrier();
+    }
+  }
+
+  main();
+}
--- add-on/popup/popup.ts
+++ add-on/popup/popup.ts
@@ -1,72 +1,74 @@
-enum PopupToggle {
-  on = 'on',
-  off = 'off',
-}
-
-class Popup {
-  storage: MyStorage;
-  elToggle = document.querySelector('#toggle')!;
-
-  get running () {
-    const sToggle = this.elToggle.getAttribute('data-bigSwitch-toggle');
-    return sToggle === PopupToggle.on;
+namespace Popup {
+  enum PopupToggle {
+    on = 'on',
+    off = 'off',
   }
 
-  set running (running: boolean) {
-    if (running !== this.running) {
-      const sToggle = running ? PopupToggle.on : PopupToggle.off;
-      this.elToggle.setAttribute('data-bigSwitch-toggle', sToggle);
+  class Popup {
+    storage: MyStorage;
+    elToggle = document.querySelector('#toggle')!;
 
-      this.onRunningChange(running);
+    get running () {
+      const sToggle = this.elToggle.getAttribute('data-bigSwitch-toggle');
+      return sToggle === PopupToggle.on;
+    }
+
+    set running (running: boolean) {
+      if (running !== this.running) {
+        const sToggle = running ? PopupToggle.on : PopupToggle.off;
+        this.elToggle.setAttribute('data-bigSwitch-toggle', sToggle);
+
+        this.onRunningChange(running);
+      }
+    }
+
+    constructor(options: { storage: MyStorage }) {
+      this.storage = options.storage;
+    }
+
+    start () {
+      this.storage.load().then((storage) => {
+        this.running = storage.running;
+      });
+
+      this.elToggle.addEventListener('click', (event) => {
+        this.toggle();
+      });
+    }
+
+    toggle () {
+      this.running = !this.running;
+    }
+
+    onRunningChange (running: boolean) {
+      this.storage.save({ running });
+
+      const text = running ? 'Running' : '';
+      browser.browserAction.setBadgeText({
+        text: text,
+      });
     }
   }
 
-  constructor(options: { storage: MyStorage }) {
-    this.storage = options.storage;
+  interface Status {
+    running: boolean
   }
 
-  start () {
-    this.storage.load().then((storage) => {
-      this.running = storage.running;
-    });
+  class MyStorage {
+    async load () {
+      // TODO find types
+      // @ts-ignore
+      return await browser.storage.local.get(['running']) as Status;
+    }
 
-    this.elToggle.addEventListener('click', (event) => {
-      this.toggle();
-    });
+    async save (status: Status) {
+      // TODO find types
+      // @ts-ignore
+      await browser.storage.local.set(status);
+    }
   }
 
-  toggle () {
-    this.running = !this.running;
-  }
-
-  onRunningChange (running: boolean) {
-    this.storage.save({ running });
-
-    const text = running ? 'Running' : '';
-    browser.browserAction.setBadgeText({
-      text: text,
-    });
-  }
+  const myStorage = new MyStorage();
+  const controller = new Popup({ storage: myStorage });
+  controller.start();
 }
-
-interface Status {
-  running: boolean
-}
-
-class MyStorage {
-  async load () {
-    // TODO find types
-    // @ts-ignore
-    return await browser.storage.local.get(['running']) as Status;
-  }
-
-  async save (status: Status) {
-    // TODO find types
-    // @ts-ignore
-    await browser.storage.local.set(status);
-  }
-}
-
-const myStorage = new MyStorage();
-const controller = new Popup({ storage: myStorage });
-controller.start();
