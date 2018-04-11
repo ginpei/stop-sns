849ab3d3f7cd180b93cbdee2bde3bb0ea0264250
ginpei <ginpei@gmail.com>
2018-04-11 11:43:47 -0700

    extract

---
+++ add-on/options_ui/OptionsUIController.ts
@@ -0,0 +1,43 @@
+class OptionsUIController {
+  private readonly elRunningRow = document.querySelector("#runningRow")!;
+  private readonly elBreakTimeLengthSec = document.querySelector("#breakTimeLength") as HTMLInputElement;
+  private readonly elReset = document.querySelector("#reset")!;
+
+  /**
+   * (milliseconds)
+   */
+  get breakTimeLength () {
+    return Number(this.elBreakTimeLengthSec.value) * 1000;
+  }
+
+  constructor (private readonly status: Status) {
+  }
+
+  public async start () {
+    this.status.onChange(() => {
+      this.render();
+    });
+
+    this.elBreakTimeLengthSec.addEventListener("input", () => {
+      const length = this.breakTimeLength;
+      if (length > 0) {
+        this.status.setBreakTimeLength(length);
+      }
+    });
+
+    this.elReset.addEventListener("click", () => {
+      this.status.reset();
+    });
+
+    await this.status.init();
+    this.render();
+  }
+
+  private render () {
+    const sRunning = this.status.running.toString();
+    this.elRunningRow.setAttribute("data-running", sRunning);
+
+    const sBreakTimeLengthSec = Math.floor(this.status.breakTimeLength / 1000).toString();
+    this.elBreakTimeLengthSec.value = sBreakTimeLengthSec;
+  }
+}
--- add-on/options_ui/main.ts
+++ add-on/options_ui/main.ts
@@ -1,43 +1,5 @@
 (async () => {
-  const elRunningRow = document.querySelector("#runningRow");
-  const elBreakTimeLengthSec = document.querySelector("#breakTimeLength") as HTMLInputElement | null;
-  const elReset = document.querySelector("#reset");
-
   const status = new Status();
-  const values = await status.init();
-
-  function update () {
-    if (!elRunningRow) {
-      throw new Error();
-    }
-    elRunningRow.setAttribute("data-running", status.running.toString());
-
-    if (!elBreakTimeLengthSec) {
-      throw new Error();
-    }
-    elBreakTimeLengthSec.value = Math.floor(status.breakTimeLength / 1000).toString();
-  }
-
-  status.onChange(() => {
-    update();
-  });
-
-  if (!elBreakTimeLengthSec) {
-    throw new Error();
-  }
-  elBreakTimeLengthSec.addEventListener("input", () => {
-    const length = Number(elBreakTimeLengthSec.value) * 1000;
-    if (length > 0) {
-      status.setBreakTimeLength(length);
-    }
-  });
-
-  if (!elReset) {
-    throw new Error();
-  }
-  elReset.addEventListener("click", () => {
-    status.reset();
-  });
-
-  update();
+  const controller = new OptionsUIController(status);
+  controller.start();
 })();
