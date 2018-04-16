087c3574132807ad80c51ebc0860cc22b9741f59
ginpei <ginpei@gmail.com>
2018-04-15 22:14:56 -0700

    chrome support

--- add-on/lib/api-polyfill.ts
+++ add-on/lib/api-polyfill.ts
@@ -4,12 +4,24 @@ if (!window.browser) {
   window.browser = window.chrome;
 }
 
-if (browser.storage.local.get.length === 1) {
+if (browser.storage.local.get.length === 0) {
   const original = browser.storage.local.get;
   // @ts-ignore
   // tslint:disable-next-line:only-arrow-functions
   browser.storage.local.get = function (keys: string[]) {
     return new Promise<browser.storage.StorageObject>((resolve, reject) => {
+      // @ts-ignore
+      original(keys, resolve);
+    });
+  };
+}
+
+if (browser.storage.local.set.length === 0) {
+  const original = browser.storage.local.set;
+  // @ts-ignore
+  // tslint:disable-next-line:only-arrow-functions
+  browser.storage.local.set = function (keys: browser.storage.StorageObject) {
+    return new Promise<void>((resolve, reject) => {
       // @ts-ignore
       original(keys, resolve);
     });
