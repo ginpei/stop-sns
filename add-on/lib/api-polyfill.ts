b8f0f3cf15195d1d02f59424d554bcf1aabacd41
ginpei <ginpei@gmail.com>
2018-04-17 11:54:27 -0700

    avoid error SCRIPT5045

--- add-on/lib/api-polyfill.ts
+++ add-on/lib/api-polyfill.ts
@@ -4,6 +4,26 @@ if (!window.browser) {
   window.browser = window.chrome;
 }
 
+// allow Edge to overwrite methods
+// (avoid "SCRIPT5045: Assignment to read-only properties is not allowed in strict mode")
+try {
+  browser.storage.local.get = browser.storage.local.get;
+} catch (error) {
+  function cloneDeep (original: any) {
+    if (typeof original !== "object" || original === null) {
+      return original;
+    }
+
+    return Object.keys(original).reduce((obj: any, prop) => {
+      obj[prop] = cloneDeep(original[prop]);
+      return obj;
+    }, {});
+  }
+
+  // @ts-ignore
+  browser = cloneDeep(browser);
+}
+
 if (browser.storage.local.get.length === 0) {
   const original = browser.storage.local.get;
   // @ts-ignore
