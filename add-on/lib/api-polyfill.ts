ba6bde6e1602a94e12f1e231da3e261f1e3dd6fe
ginpei <ginpei@gmail.com>
2018-04-17 20:02:59 -0700

    replace own polyfill with webextension-polyfill

--- add-on/lib/api-polyfill.ts
+++ add-on/lib/api-polyfill.ts
@@ -1,13 +1,10 @@
-// @ts-ignore
-if (!window.browser) {
-  // @ts-ignore
-  window.browser = window.chrome;
-}
-
 // allow Edge to overwrite methods
 // (avoid "SCRIPT5045: Assignment to read-only properties is not allowed in strict mode")
 try {
-  browser.storage.local.get = browser.storage.local.get;
+  // @ts-ignore
+  if (window.browser) {
+    browser.storage.local.get = browser.storage.local.get;
+  }
 } catch (error) {
   function cloneDeep (original: any) {
     if (typeof original !== "object" || original === null) {
@@ -20,30 +17,11 @@ try {
     }, {});
   }
 
+  // allow overwrite
   // @ts-ignore
-  browser = cloneDeep(browser);
-}
+  window.chrome = cloneDeep(window.browser);
 
-if (browser.storage.local.get.length === 0) {
-  const original = browser.storage.local.get;
+  // pretend Chrome
   // @ts-ignore
-  // tslint:disable-next-line:only-arrow-functions
-  browser.storage.local.get = function (keys: string[]) {
-    return new Promise<browser.storage.StorageObject>((resolve, reject) => {
-      // @ts-ignore
-      original(keys, resolve);
-    });
-  };
-}
-
-if (browser.storage.local.set.length === 0) {
-  const original = browser.storage.local.set;
-  // @ts-ignore
-  // tslint:disable-next-line:only-arrow-functions
-  browser.storage.local.set = function (keys: browser.storage.StorageObject) {
-    return new Promise<void>((resolve, reject) => {
-      // @ts-ignore
-      original(keys, resolve);
-    });
-  };
+  window.browser = undefined;
 }
